0d405ac039708550f95dbd8716b7cc7969a3be11
Zeynel <53123586+ZeynelAkin@users.noreply.github.com>
2022-06-17 18:08:09 +0300

    Add files via upload

---
+++ Back-and Challange zeynel akın/challange/src/main/java/enoca/challange/zeynel/ChallangeApplication.java
@@ -0,0 +1,14 @@
+package enoca.challange.zeynel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallangeApplication.class, args);
+
+	}
+
+}
---
+++ Back-and Challange zeynel akın/challange/src/test/java/enoca/challange/zeynel/ChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package enoca.challange.zeynel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
