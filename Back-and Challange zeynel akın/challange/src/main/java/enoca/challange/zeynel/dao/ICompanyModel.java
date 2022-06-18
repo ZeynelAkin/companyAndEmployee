31913055aa46614b4ac203b7739562970152b647
Zeynel <53123586+ZeynelAkin@users.noreply.github.com>
2022-06-18 16:14:08 +0300

    Delete Back-and Challange zeynel akın directory

--- Back-and Challange zeynel akın/challange/src/main/java/enoca/challange/zeynel/dao/ICompanyModel.java
+++
@@ -1,8 +0,0 @@
-package enoca.challange.zeynel.dao;
-
-import enoca.challange.zeynel.model.CompanyModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ICompanyModel extends JpaRepository<CompanyModel,Integer> {
-
-}
