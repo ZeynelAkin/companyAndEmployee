d3f4520a6dace98604117e62320d1afd77a6fe83
Zeynel <53123586+ZeynelAkin@users.noreply.github.com>
2022-06-18 16:11:35 +0300

    Delete companyAndEmployee directory

--- companyAndEmployee/challange/src/main/java/enoca/challange/zeynel/dao/IEmployeesModel.java
+++
@@ -1,7 +0,0 @@
-package enoca.challange.zeynel.dao;
-
-import enoca.challange.zeynel.model.EmployeesModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IEmployeesModel extends JpaRepository<EmployeesModel,Integer> {
-}
